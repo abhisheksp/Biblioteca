7a80d285992f8a7c6aa28971c3b5d8bfc4a2a5d4
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-04 12:37:32 +0530

    Changed Previous Design, Added WelcomeMessage Spec to Display Welcome Message

---
+++ src/com/thoughtworks/biblioteca/WelcomeMessage.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.biblioteca;
+
+/* WelcomeMesssage takes a WelcomeMessage and displays it */
+public class WelcomeMessage {
+    private String message;
+
+    public WelcomeMessage(String message) {
+        this.message = message;
+    }
+
+    public void display() {
+        System.out.print(message);
+    }
+}

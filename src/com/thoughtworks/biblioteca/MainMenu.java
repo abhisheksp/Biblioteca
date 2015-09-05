bf756c111f1bd07a3cfa27cdd021d1508af1f9a4
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-05 12:11:32 +0530

    Refactored and added MainTestTest Spec

---
+++ src/com/thoughtworks/biblioteca/MainMenu.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.biblioteca;
+
+/**
+ * Created by abhisp on 05/09/15.
+ */
+public class MainMenu {
+}
---
+++ test/com/thoughtworks/biblioteca/MainMenuTest.java
@@ -0,0 +1,38 @@
+package com.thoughtworks.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MainMenuTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setupStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(System.out);
+    }
+
+
+    @Test
+    public void shouldDisplayListBooksOptionInitially(){
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        MainMenu mainMenu= new MainMenu(options);
+
+        mainMenu.displayOptions();
+
+        assertEquals("1. List Books\n", outContent.toString());
+    }
+}

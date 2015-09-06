75fb3cc935f647dd9baf650904825a742dae39b1
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-06 14:57:03 +0530

    Added MainMenuSpec, should delegate control to bookList.display when option 1 is chosen

--- src/com/thoughtworks/biblioteca/MainMenu.java
+++ src/com/thoughtworks/biblioteca/MainMenu.java
@@ -1,20 +1,24 @@
 package com.thoughtworks.biblioteca;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MainMenu {
 
     private ArrayList<String> options;
     private InputReader inputReader;
+    private Map<String, Option> optionsMap;
 
-    public MainMenu(ArrayList<String> options, InputReader inputReader) {
+    public MainMenu(ArrayList<String> options, InputReader inputReader, Map<String, Option> optionsMap) {
         this.options = options;
         this.inputReader = inputReader;
+        this.optionsMap = optionsMap;
     }
 
     public void interactWithUser() {
-        inputReader.read();
         displayOptions();
+        String input = inputReader.read();
+        optionsMap.get(input).display();
     }
 
     private void displayOptions() {
--- test/com/thoughtworks/biblioteca/MainMenuTest.java
+++ test/com/thoughtworks/biblioteca/MainMenuTest.java
@@ -3,9 +3,14 @@ package com.thoughtworks.biblioteca;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -30,7 +35,8 @@ public class MainMenuTest {
         ArrayList<String> options = new ArrayList<String>();
         options.add("1. List Books");
         InputReader inputReader = mock(InputReader.class);
-        MainMenu mainMenu = new MainMenu(options, inputReader);
+        Map<String, Option> optionsMap = new HashMap<String, Option>();
+        MainMenu mainMenu = new MainMenu(options, inputReader, optionsMap);
 
         mainMenu.interactWithUser();
 
@@ -42,11 +48,37 @@ public class MainMenuTest {
         ArrayList<String> options = new ArrayList<String>();
         options.add("1. List Books");
         InputReader inputReader = mock(InputReader.class);
-        MainMenu mainMenu = new MainMenu(options, inputReader);
+        Map<String, Option> optionsMap = new HashMap<String, Option>();
+        MainMenu mainMenu = new MainMenu(options, inputReader, optionsMap);
 
         mainMenu.interactWithUser();
 
         verify(inputReader).read();
         assertEquals("1. List Books\n", outContent.toString());
+    }
+
+    @Test
+    public void shouldDelegateToBookListDisplayWhenOptionOneIsChosen() {
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        String input = "1";
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+        Scanner scanner = new Scanner(System.in);
+        InputReader inputReader = new InputReader(scanner);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Brief History of Time", "Stephen Hawking", 1988));
+        books.add(new Book("Crime and Punishment", "Fyodor Dostoyevsky", 1866));
+        books.add(new Book("Seven Minutes", "Irving Wallace", 1969));
+        BookList bookList = new BookList(books);
+        Map<String, Option> optionsMap = new HashMap<String, Option>();
+        optionsMap.put("1", bookList);
+        MainMenu mainMenu = new MainMenu(options, inputReader, optionsMap);
+
+        mainMenu.interactWithUser();
+
+        assertEquals("1. List Books\n"+"Book\tAuthor\tYear Published\n"+"Brief History of Time\tStephen Hawking\t1988\n"+
+                "Crime and Punishment\tFyodor Dostoyevsky\t1866\n"+
+                "Seven Minutes\tIrving Wallace\t1969\n", outContent.toString());
     }
 }

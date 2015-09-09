2937ab7485bbf714ea53381cea4209ff08bc7725
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-09 21:46:45 +0530

    Refactored Parser

--- src/com/thoughtworks/biblioteca/Parser.java
+++ src/com/thoughtworks/biblioteca/Parser.java
@@ -1,27 +1,32 @@
 package com.thoughtworks.biblioteca;
 
+import java.util.HashMap;
+
 public class Parser {
 
     private String input;
     private InputReader inputReader;
     private Library library;
+    private HashMap<String, MenuOption> menuOptionsMap;
 
     public Parser(String input, InputReader inputReader, Library library) {
         this.input = input;
         this.inputReader = inputReader;
         this.library = library;
+        configureMenuOptionsMap();
+    }
+
+    private void configureMenuOptionsMap() {
+        menuOptionsMap = new HashMap<String, MenuOption>();
+        menuOptionsMap.put("1", new ListBooksMenuOption(library));
+        menuOptionsMap.put("2", new QuitMenuOption());
+        menuOptionsMap.put("3", new CheckoutBookMenuOption(inputReader, library));
+        menuOptionsMap.put("4", new CheckInBookMenuOption(inputReader, library));
+        menuOptionsMap.put("invalid", new InvalidMenuOption("Select a valid option!"));
     }
 
     public MenuOption parse() {
-        if(input.equals("1"))
-            return new ListBooksMenuOption(library);
-        else if(input.equals("2"))
-            return new QuitMenuOption();
-        else if(input.equals("3"))
-            return new CheckoutBookMenuOption(inputReader, library);
-        else if(input.equals("4"))
-            return new CheckInBookMenuOption(inputReader, library);
-        else
-            return new InvalidMenuOption("Select a valid option!");
+        MenuOption menuOption = menuOptionsMap.get(input);
+        return menuOption == null ? menuOptionsMap.get("invalid") : menuOption;
     }
 }

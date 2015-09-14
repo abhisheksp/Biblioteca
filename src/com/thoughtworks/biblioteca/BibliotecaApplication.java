6f95740113f47d51b0fa6deec0b136b1c44b5157
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-14 14:05:54 +0530

    Refactored Biblioteca Application , extratced options

--- src/com/thoughtworks/biblioteca/BibliotecaApplication.java
+++ src/com/thoughtworks/biblioteca/BibliotecaApplication.java
@@ -9,13 +9,18 @@ public class BibliotecaApplication {
         InputReader inputReader = new InputReaderFactory().getDefaultInputReader();
         Library library = new LibraryFactory().getDefaultLibrary();
         Parser parser = new Parser(inputReader, library);
+        ArrayList<String> options = getOptions();
+        MainMenu mainMenu = new MainMenu(options, inputReader, parser);
+        Controller controller = new Controller(welcomeMessage, mainMenu);
+        controller.start();
+    }
+
+    private ArrayList<String> getOptions() {
         ArrayList<String> options = new ArrayList<String>();
         options.add("1. List Books");
         options.add("2. Quit");
         options.add("3. Checkout Book");
         options.add("4. Checkin Book");
-        MainMenu mainMenu = new MainMenu(options, inputReader, parser);
-        Controller controller = new Controller(welcomeMessage, mainMenu);
-        controller.start();
+        return options;
     }
 }

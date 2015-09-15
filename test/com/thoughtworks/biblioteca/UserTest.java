40a9662d6c2f7e58fdedd7a0fed2f19b778f9dd1
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-15 14:52:09 +0530

    Added User Spec, User should not be equal to another user wirh different credentials

--- test/com/thoughtworks/biblioteca/UserTest.java
+++ test/com/thoughtworks/biblioteca/UserTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.biblioteca;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class UserTest {
 
@@ -17,5 +18,11 @@ public class UserTest {
     public void shouldBeEqualToAnotherUserWithSameCredentials(){
 
         assertEquals(new User("123-4567", "password"), new User("123-4567", "password"));
+    }
+
+    @Test
+    public void shouldNotBeEqualToAnotherUserWithDifferentCredentials(){
+
+        assertNotEquals(new User("123-4567", "password"), new User("222-2222", "meh"));
     }
 }

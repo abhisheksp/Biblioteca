4aaf66d63492f5f149b8a21d0aea1fed4ac01e8d
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-15 14:16:12 +0530

    Added CheckOutMovieMenuOption Spec, should display success message after succesful movie checkout

--- src/com/thoughtworks/biblioteca/CheckOutMovieMenuOption.java
+++ src/com/thoughtworks/biblioteca/CheckOutMovieMenuOption.java
@@ -8,15 +8,17 @@ public class CheckOutMovieMenuOption {
 
     private InputReader inputReader;
     private MovieLibrary movieLibrary;
+    private ConsoleDisplayFactory consoleDisplayFactory;
 
-
-    public CheckOutMovieMenuOption(InputReader inputReader, MovieLibrary movieLibrary) {
+    public CheckOutMovieMenuOption(InputReader inputReader, MovieLibrary movieLibrary, ConsoleDisplayFactory consoleDisplayFactory) {
         this.inputReader = inputReader;
         this.movieLibrary = movieLibrary;
+        this.consoleDisplayFactory = consoleDisplayFactory;
     }
 
     public void doOperation() {
         String movieName = inputReader.read();
-        movieLibrary.checkOut(new Movie(movieName, UNKNOWN_YEAR, UNKNOWN_DIRECTOR, UNKNOWN_RATING));
+        if(movieLibrary.checkOut(new Movie(movieName, UNKNOWN_YEAR, UNKNOWN_DIRECTOR, UNKNOWN_RATING)))
+            consoleDisplayFactory.getNewConsoleDisplay("Thank you! Enjoy the book").display();
     }
 }
--- test/com/thoughtworks/biblioteca/CheckOutMovieMenuOptionTest.java
+++ test/com/thoughtworks/biblioteca/CheckOutMovieMenuOptionTest.java
@@ -1,18 +1,39 @@
 package com.thoughtworks.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class CheckOutMovieMenuOptionTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setupStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(System.out);
+    }
 
     @Test
     public void shouldCallInputReaderReadWhenDoOperationIsCalled() {
         InputReader inputReader = mock(InputReader.class);
         MovieLibrary movieLibrary = mock(MovieLibrary.class);
-        CheckOutMovieMenuOption checkOutMovieMenuOption = new CheckOutMovieMenuOption(inputReader, movieLibrary);
+        ConsoleDisplayFactory consoleDisplayFactory = mock(ConsoleDisplayFactory.class);
+        CheckOutMovieMenuOption checkOutMovieMenuOption = new CheckOutMovieMenuOption(inputReader, movieLibrary, consoleDisplayFactory);
 
         checkOutMovieMenuOption.doOperation();
 
@@ -23,11 +44,27 @@ public class CheckOutMovieMenuOptionTest {
     public void shouldCallMovieLibraryCheckoutWithGivenMovieWhenDoOperationIsCalled() {
         InputReader inputReader = mock(InputReader.class);
         MovieLibrary movieLibrary = mock(MovieLibrary.class);
-        CheckOutMovieMenuOption checkOutMovieMenuOption = new CheckOutMovieMenuOption(inputReader, movieLibrary);
+        ConsoleDisplayFactory consoleDisplayFactory = mock(ConsoleDisplayFactory.class);
+        CheckOutMovieMenuOption checkOutMovieMenuOption = new CheckOutMovieMenuOption(inputReader, movieLibrary, consoleDisplayFactory);
         ArgumentCaptor<Movie> argumentCaptor = ArgumentCaptor.forClass(Movie.class);
 
         checkOutMovieMenuOption.doOperation();
 
         verify(movieLibrary).checkOut(argumentCaptor.capture());
+    }
+
+    @Test
+    public void shouldDisplaySuccessAfterSuccessfulCheckout() {
+        InputReader inputReader = mock(InputReader.class);
+        MovieLibrary movieLibrary = mock(MovieLibrary.class);
+        ConsoleDisplayFactory consoleDisplayFactory = mock(ConsoleDisplayFactory.class);
+        CheckOutMovieMenuOption checkOutMovieMenuOption = new CheckOutMovieMenuOption(inputReader, movieLibrary, consoleDisplayFactory);
+        ArgumentCaptor<Movie> argumentCaptor = ArgumentCaptor.forClass(Movie.class);
+
+        when(consoleDisplayFactory.getNewConsoleDisplay(anyString())).thenReturn(new ConsoleDisplay("Thank you! Enjoy the movie"));
+        when(movieLibrary.checkOut(argumentCaptor.capture())).thenReturn(true);
+        checkOutMovieMenuOption.doOperation();
+
+        assertEquals("Thank you! Enjoy the movie\n", outContent.toString());
     }
 }

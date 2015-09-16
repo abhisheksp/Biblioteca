b39b194085d5f04379e34990b593a62ccaee425b
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-16 14:25:38 +0530

    Added Menu Factory Spec, should return librarian menu when librarian is passed

--- test/com/thoughtworks/biblioteca/MainMenuFactoryTest.java
+++ test/com/thoughtworks/biblioteca/MainMenuFactoryTest.java
@@ -89,4 +89,22 @@ public class MainMenuFactoryTest {
 
         assertEquals(mainMenu, mainMenuFactory.getMenu(new User("222-2222", "juliusseizure", "user")));
     }
+
+    @Test
+    public void shouldReturnLibrarianMenuWhenLibrarianUserIsPassed(){
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        options.add("2. List Movies");
+        options.add("3. Checkout Book");
+        options.add("4. Checkin Book");
+        options.add("5. Checkout Movie");
+        options.add("6. Book Status");
+        options.add("7. Logout");
+        options.add("8. Quit");
+        ConsoleDisplayFactory consoleDisplayFactory = new ConsoleDisplayFactory();
+        MainMenu mainMenu = new MainMenu(options, consoleDisplayFactory);
+        MainMenuFactory mainMenuFactory = new MainMenuFactory();
+
+        assertEquals(mainMenu, mainMenuFactory.getMenu(new User("222-2222", "juliusseizure", "librarian")));
+    }
 }

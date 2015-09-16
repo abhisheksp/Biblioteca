0c66d6ca8de79488e4a8a668fecd37239d886d97
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-16 13:13:23 +0530

    Added mainmenufactory spec, should return user menu when user menu is called

--- src/com/thoughtworks/biblioteca/MainMenuFactory.java
+++ src/com/thoughtworks/biblioteca/MainMenuFactory.java
@@ -12,7 +12,20 @@ public class MainMenuFactory {
         options.add("2. List Movies");
         options.add("3. Checkout Movie");
         options.add("4. Login");
-        options.add("5. Checkout Movie");
+        options.add("5. Quit");
+        mainMenu =  new MainMenu(options, new ConsoleDisplayFactory());
+        return mainMenu;
+    }
+
+    public MainMenu userMenu() {
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        options.add("2. List Movies");
+        options.add("3. Checkout Book");
+        options.add("4. Checkin Book");
+        options.add("3. Checkout Movie");
+        options.add("4. Logout");
+        options.add("6. Quit");
         mainMenu =  new MainMenu(options, new ConsoleDisplayFactory());
         return mainMenu;
     }
--- test/com/thoughtworks/biblioteca/MainMenuFactoryTest.java
+++ test/com/thoughtworks/biblioteca/MainMenuFactoryTest.java
@@ -15,11 +15,28 @@ public class MainMenuFactoryTest {
         options.add("2. List Movies");
         options.add("3. Checkout Movie");
         options.add("4. Login");
-        options.add("5. Checkout Movie");
+        options.add("5. Quit");
         ConsoleDisplayFactory consoleDisplayFactory = new ConsoleDisplayFactory();
         MainMenu mainMenu = new MainMenu(options, consoleDisplayFactory);
         MainMenuFactory mainMenuFactory = new MainMenuFactory();
 
         assertEquals(mainMenu, mainMenuFactory.guestMenu());
+    }
+
+    @Test
+    public void shouldReturnUserMenuWhenUserMenuIsCalled(){
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        options.add("2. List Movies");
+        options.add("3. Checkout Book");
+        options.add("4. Checkin Book");
+        options.add("3. Checkout Movie");
+        options.add("4. Logout");
+        options.add("6. Quit");
+        ConsoleDisplayFactory consoleDisplayFactory = new ConsoleDisplayFactory();
+        MainMenu mainMenu = new MainMenu(options, consoleDisplayFactory);
+        MainMenuFactory mainMenuFactory = new MainMenuFactory();
+
+        assertEquals(mainMenu, mainMenuFactory.userMenu());
     }
 }

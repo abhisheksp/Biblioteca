2ba0dc738eb4d3ddbda75b3e5ff76167bbb00f14
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-16 14:19:51 +0530

    Added Menu Factory Spec, should return guest menu when guest user is passed

--- src/com/thoughtworks/biblioteca/MainMenuFactory.java
+++ src/com/thoughtworks/biblioteca/MainMenuFactory.java
@@ -6,6 +6,15 @@ public class MainMenuFactory {
 
     private MainMenu mainMenu;
 
+    public MainMenu getMenu(User user){
+        if(user.role().equals("librarian"))
+            return librarianMenu();
+        else if(user.role().equals("user"))
+            return userMenu();
+        else
+            return guestMenu();
+    }
+
     public MainMenu guestMenu(){
         ArrayList<String> options = new ArrayList<String>();
         options.add("1. List Books");
@@ -23,9 +32,23 @@ public class MainMenuFactory {
         options.add("2. List Movies");
         options.add("3. Checkout Book");
         options.add("4. Checkin Book");
-        options.add("3. Checkout Movie");
-        options.add("4. Logout");
-        options.add("6. Quit");
+        options.add("5. Checkout Movie");
+        options.add("6. Logout");
+        options.add("7. Quit");
+        mainMenu =  new MainMenu(options, new ConsoleDisplayFactory());
+        return mainMenu;
+    }
+
+    public MainMenu librarianMenu() {
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        options.add("2. List Movies");
+        options.add("3. Checkout Book");
+        options.add("4. Checkin Book");
+        options.add("5. Checkout Movie");
+        options.add("6. Book Status");
+        options.add("7. Logout");
+        options.add("8. Quit");
         mainMenu =  new MainMenu(options, new ConsoleDisplayFactory());
         return mainMenu;
     }
--- test/com/thoughtworks/biblioteca/MainMenuFactoryTest.java
+++ test/com/thoughtworks/biblioteca/MainMenuFactoryTest.java
@@ -30,13 +30,46 @@ public class MainMenuFactoryTest {
         options.add("2. List Movies");
         options.add("3. Checkout Book");
         options.add("4. Checkin Book");
-        options.add("3. Checkout Movie");
-        options.add("4. Logout");
-        options.add("6. Quit");
+        options.add("5. Checkout Movie");
+        options.add("6. Logout");
+        options.add("7. Quit");
         ConsoleDisplayFactory consoleDisplayFactory = new ConsoleDisplayFactory();
         MainMenu mainMenu = new MainMenu(options, consoleDisplayFactory);
         MainMenuFactory mainMenuFactory = new MainMenuFactory();
 
         assertEquals(mainMenu, mainMenuFactory.userMenu());
+    }
+
+    @Test
+    public void shouldReturnLibrarianMenuWhenLibrarianMenuIsCalled(){
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        options.add("2. List Movies");
+        options.add("3. Checkout Book");
+        options.add("4. Checkin Book");
+        options.add("5. Checkout Movie");
+        options.add("6. Book Status");
+        options.add("7. Logout");
+        options.add("8. Quit");
+        ConsoleDisplayFactory consoleDisplayFactory = new ConsoleDisplayFactory();
+        MainMenu mainMenu = new MainMenu(options, consoleDisplayFactory);
+        MainMenuFactory mainMenuFactory = new MainMenuFactory();
+
+        assertEquals(mainMenu, mainMenuFactory.librarianMenu());
+    }
+
+    @Test
+    public void shouldReturnGuestMenuWhenGuestUserIsPassed(){
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("1. List Books");
+        options.add("2. List Movies");
+        options.add("3. Checkout Movie");
+        options.add("4. Login");
+        options.add("5. Quit");
+        ConsoleDisplayFactory consoleDisplayFactory = new ConsoleDisplayFactory();
+        MainMenu mainMenu = new MainMenu(options, consoleDisplayFactory);
+        MainMenuFactory mainMenuFactory = new MainMenuFactory();
+
+        assertEquals(mainMenu, mainMenuFactory.getMenu(new User("", "", "guest")));
     }
 }

8a9e447ffc11639e6ea8cc16b28a0366a676179d
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-16 16:29:38 +0530

    Added Parser Spec, should return checkout movie menu option when current user is a librarian  and input is five

--- src/com/thoughtworks/biblioteca/Parser.java
+++ src/com/thoughtworks/biblioteca/Parser.java
@@ -36,7 +36,8 @@ public class Parser {
         menuOptionsMap.put("1", new ListBooksMenuOption(library, consoleDisplayFactory));
         menuOptionsMap.put("2", new ListMoviesMenuOption(movieLibrary, consoleDisplayFactory));
         menuOptionsMap.put("3", new CheckoutBookMenuOption(inputReader, library));
-        menuOptionsMap.put("4", new CheckInBookMenuOption(inputReader, library));   
+        menuOptionsMap.put("4", new CheckInBookMenuOption(inputReader, library));
+        menuOptionsMap.put("5", new CheckOutMovieMenuOption(inputReader, movieLibrary, consoleDisplayFactory));
     }
 
     private void configureUsertMenuOptions() {

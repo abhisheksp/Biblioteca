ed12a72debb6e507f1ad2d978aeac9b3d40a7775
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-17 22:38:33 +0530

    Refactored Parser Factory

--- src/com/thoughtworks/biblioteca/ParserFactory.java
+++ src/com/thoughtworks/biblioteca/ParserFactory.java
@@ -11,18 +11,10 @@ public class ParserFactory {
     public ParserFactory() {
         this.inputReader = new InputReaderFactory().getDefaultInputReader();
         this.consoleDisplayFactory = new ConsoleDisplayFactory();
-        this.authenticator = new Authenticator(getRegisteredUsers(), consoleDisplayFactory);
+        this.authenticator = new AuthenticatorFactory().getDefaultAuthenticator();
     }
 
     public Parser getNewParser(Library library, MovieLibrary movieLibrary, User currentUser) {
         return new Parser(inputReader, library, movieLibrary, consoleDisplayFactory, currentUser, authenticator);
-    }
-
-    private ArrayList<User> getRegisteredUsers() {
-        ArrayList<User> users = new ArrayList<User>();
-        users.add(new User("666-2222", "blacksheep", "librarian", "Voldemort", "voldemort@killharry.com", "999999999"));
-        users.add(new User("222-2222", "juliusseizure", "user", "Julius Caesar", "julius@caesar.com", "888888888"));
-        users.add(new User("888-2222", "murica", "user", "Barack Obama", "obama@us.gov", "555555555"));
-        return users;
     }
 }
